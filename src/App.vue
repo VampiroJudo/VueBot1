147be7a862db8735de64f954f9ab51d2c4c7b7b6
John Conley <johnconley507@hotmail.com>
2019-04-05 09:10:18 -0400

    initialing build a bot vue project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <main>
+      <HomePage />
+    </main>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width:1024px;
+  min-height: 300px;
+}
+</style>
